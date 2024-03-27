2ea57b8caaa27b27e5280a6a94fe60b9e8b71146
Ondrej Sika <ondrej@ondrejsika.com>
2024-03-27 14:32:56 +0100

    ci: Add test stage

--- generate-gitlab-ci.py
+++ generate-gitlab-ci.py
@@ -13,20 +13,28 @@ def make_service(name):
             "stage": "build",
             "script":[
                 "echo Build %s" % name,
-            ]
+            ],
+        },
+        "test-%s" % name: {
+            "stage": "test",
+            "script":[
+                "echo Build %s" % name,
+            ],
+            "needs": ["build-%s" % name]
         },
         "deploy-%s" % name: {
             "stage": "deploy",
             "script":[
                 "echo Deploy %s" % name,
-            ]
+            ],
+            "needs": ["test-%s" % name],
         }
     }
 
 with open(".gitlab-ci.yml", "w") as f:
     pipeline = {}
     pipeline.update({
-        "stages": ["build", "deploy"]
+        "stages": ["build", "test", "deploy"]
     })
     for service in SERVICES:
         pipeline.update(make_service(service))
